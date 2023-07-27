194d6b8ad5f942e303b690b4e4c11e1ce10668f2
sahebbali <sahebbali253@gmail.comm>
2023-07-27 12:52:38 +0600

    add someone

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
+import fetch from 'node-fetch';
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch("/api/posts", {
     cache: "no-store",
   });
 
